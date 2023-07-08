2652888d79f7fdee96f5cf31d3a326f5e7f0fc07
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-08 14:52:28 +0600

    VitaMax 5

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,13 +2,30 @@ import React from "react";
 
 import Header from "../components/Header";
 
+import illustration from "../assets/amico.svg";
 import "../styles/main-screen/main-screen.css";
 
 const MainScreen = () => {
   return (
     <div className="main-screen">
       <Header />
-      <div className="container"></div>
+      <div className="main-content">
+        <div className="container">
+          <img className="illustration" src={illustration}></img>
+          <div className="heading-container">
+            <h1 className="heading">«ВитаМакс Фарм» </h1>
+            <span className="black">
+              Маркетингово-дистрибьюторская компания
+            </span>
+          </div>
+          <p className="main-paragraph">
+            Обладает комплексом современных инструментов продвижения и обширным
+            опытом работы в продажах лекарственных препаратов, БАДов, косметики
+            и изделий медицинского назначения.
+          </p>
+          <button className="contact-us-btn">Связаться с нами</button>
+        </div>
+      </div>
     </div>
   );
 };
