3d3dbe7719791e183d1636fb6f846b23f5cab0e9
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-08 21:58:08 +0600

    VitaMax12

--- src/screens/ConsultingScreen.tsx
+++ src/screens/ConsultingScreen.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import mark from "../assets/mark.svg";
 import consulting from "../assets/consulting.png";
 import "../styles/consulting-screen/consulting-screen.css";
 
+interface MarksProps {
+  propsName: string;
+}
+
 const ConsultingSreen = () => {
+  const MarkComponents: React.FC<MarksProps> = (props) => {
+    return (
+      <div className="mark-container">
+        <figure>
+          <img className="mark" src={mark} alt="mark"></img>
+        </figure>
+        <span className="mark-text">{props.propsName}</span>
+      </div>
+    );
+  };
+
   return (
     <div className="consulting-screen">
       <div className="container">
@@ -17,6 +33,23 @@ const ConsultingSreen = () => {
             <strong className="green">Консалтинговые услуги</strong> в области
             фармацевтики
           </h2>
+          <div className="marks">
+            <MarkComponents propsName="Анализ рынка"></MarkComponents>
+            <MarkComponents propsName="Прогнозирование и моделирование продаж"></MarkComponents>
+            <MarkComponents propsName="Поиск объектов экстенсивного развития"></MarkComponents>
+            <MarkComponents
+              propsName="Полное сопровождение сделок M&A,
+              in&out- licensing"
+            ></MarkComponents>
+            <MarkComponents propsName="Ценообразование"></MarkComponents>
+            <MarkComponents
+              propsName="Разработка Market access стратегий
+              "
+            ></MarkComponents>
+            <MarkComponents propsName="Коммерческая эффективность"></MarkComponents>
+            <MarkComponents propsName="Оценка здоровья бизнеса"></MarkComponents>
+            <MarkComponents propsName="Разбитие бизнеса"></MarkComponents>
+          </div>
         </section>
       </div>
     </div>
---
+++ src/screens/PartnersScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PartnersScreen = () => {
+  return <div className="partenrs-screen">PartnersScreen</div>;
+};
+
+export default PartnersScreen;
