44cda4db0bdf564214b7330d4ab6f33942151856
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-10 20:41:07 +0600

    VitaMax 17

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,10 +9,14 @@ const Footer = () => {
       <img className="logo-white" src={logoWhite} alt="logo-white"></img>
       <nav className="nav-links">
         <a className="link">О компании</a>
-        <a className="link">Команда</a>
-        <a className="link">Партнеры</a>
-        <a className="link">Лицензия</a>
+        <a className="link main">Команда</a>
+        <a className="link main">Партнеры</a>
+        <a className="link main">Лицензия</a>
       </nav>
+      <span className="line"></span>
+      <p className="footer-paragraph">
+        Copyright ® 2021 Company All rights Rcerved
+      </p>
     </footer>
   );
 };
