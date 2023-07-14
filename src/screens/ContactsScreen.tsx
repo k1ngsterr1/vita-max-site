a576c2a6581a42eb22b26c20c540d69d82e90e55
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-14 14:53:18 +0600

    VitaMax31

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -27,6 +27,26 @@ const ContactsScreen = () => {
           </div>
         </div>
       </section>
+      <section className="web-contacts-section">
+        <h2 className="contacts-heading">Контакты</h2>
+        <p className="contacts-p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        <form className="contacts-form">
+          <div className="text-container">
+            <h3 className="contact-us-heading">Оставайтесь с нами</h3>
+            <p className="contact-p">
+              Мы сами напишем вам для дальнейшего сотрудничестве, будь в курсе
+              всех событий и получайте новейшие обновления.
+            </p>
+          </div>
+          <div className="input-container">
+            <input className="input" placeholder="Введите ваш email"></input>
+            <button className="send-btn">Отправить</button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 };
