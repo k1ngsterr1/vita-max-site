7b8c7d6b54824849270caf0c9b7d60b3bc917bb4
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-14 16:16:25 +0600

    VitaMax

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,20 +3,51 @@ import React from "react";
 import "../styles/footer/footer.css";
 import logoWhite from "../assets/logoWhite.svg";
 
+import { Link } from "react-scroll";
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <img className="logo-white" src={logoWhite} alt="logo-white"></img>
-      <nav className="nav-links">
-        <a className="link">О компании</a>
-        <a className="link main">Команда</a>
-        <a className="link main">Партнеры</a>
-        <a className="link main">Лицензия</a>
-      </nav>
-      <span className="line"></span>
-      <p className="footer-paragraph">
-        Copyright ® 2021 Company All rights Rcerved
-      </p>
+      <div className="footer-phone">
+        <img className="logo-white" src={logoWhite} alt="logo-white"></img>
+        <nav className="nav-links">
+          <Link className="link" to="about">
+            О компании
+          </Link>
+          <Link className="link main" to="team">
+            Команда
+          </Link>
+          <Link className="link main" to="partnership">
+            Партнеры
+          </Link>
+          <Link className="link main" to="license">
+            Лицензия
+          </Link>
+        </nav>
+        <span className="line"></span>
+        <p className="footer-paragraph">
+          Copyright ® 2021 Company All rights Rcerved
+        </p>
+      </div>
+      <div className="footer-web">
+        <div className="footer-content">
+          <img className="vitamax-logo" src={logoWhite} alt="logo"></img>
+          <nav className="nav-links">
+            <Link className="link" to="about">
+              О компании
+            </Link>
+            <Link className="link main" to="team">
+              Команда
+            </Link>
+            <Link className="link main" to="partnership">
+              Партнеры
+            </Link>
+            <Link className="link main" to="license">
+              Лицензия
+            </Link>
+          </nav>
+        </div>
+      </div>
     </footer>
   );
 };
