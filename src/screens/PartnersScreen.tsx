c110c5f9bea50b4fe96b3be769a20d2d55c0e6e2
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-18 14:47:26 +0600

    VitaMax40

--- src/screens/PartnersScreen.tsx
+++ src/screens/PartnersScreen.tsx
@@ -4,6 +4,7 @@ import logo1 from "../assets/logo1.png";
 import GenSci from "../assets/GenSci.png";
 import logo2 from "../assets/logo2.png";
 import ProfitPharm from "../assets/profitpharm.png";
+import Kobayashi from "../assets/kobayashi.png";
 import logo3 from "../assets/logo3.png";
 import logo4 from "../assets/logo4.png";
 import logo5 from "../assets/logo5.png";
@@ -17,11 +18,33 @@ const PartnersScreen = () => {
         <h2 className="partners-heading">Партнеры</h2>
         <div className="logos">
           <div className="logo-upper">
-            <img className="logo1" src={logo1} alt="logo1"></img>
-            <img className="logo2" src={logo2} alt="logo2"></img>
+            <img
+              className="logo1"
+              onClick={() =>
+                window.open("https://www.gensci-china.com/", "_blank")
+              }
+              src={GenSci}
+              alt="logo1"
+            ></img>
+            <img
+              className="logo2"
+              src={ProfitPharm}
+              alt="logo2"
+              onClick={() => window.open("https://profitpharm.ru/", "_blank")}
+            ></img>
           </div>
           <div className="logo-lower">
-            <img className="logo3" src={logo3} alt="logo3"></img>
+            <img
+              className="logo3"
+              src={Kobayashi}
+              alt="logo3"
+              onClick={() =>
+                window.open(
+                  "https://www.kobayashi.co.jp/english/index.html",
+                  "_blank"
+                )
+              }
+            ></img>
             <img className="logo4" src={logo4} alt="logo4"></img>
           </div>
         </div>
@@ -43,7 +66,17 @@ const PartnersScreen = () => {
             alt="logo2"
             onClick={() => window.open("https://profitpharm.ru/", "_blank")}
           ></img>
-          <img className="logo3" src={logo3} alt="logo3"></img>
+          <img
+            className="logo3"
+            src={Kobayashi}
+            alt="logo3"
+            onClick={() =>
+              window.open(
+                "https://www.kobayashi.co.jp/english/index.html",
+                "_blank"
+              )
+            }
+          ></img>
           <img className="logo4" src={logo4} alt="logo4"></img>
           <img className="logo5" src={logo5} alt="logo5"></img>
         </div>
